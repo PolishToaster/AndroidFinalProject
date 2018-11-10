ef4f2af222d450f2a90db25114df334ceacff11e
zackt-laptop <inkwellanimations@gmail.com>
2018-11-10 14:14:00 -0500

    added oc transpo activity and updated the main activity button to launch oc transpo activity

--- app/src/main/java/com/algonquin/androidfinalproject/MainActivity.java
+++ app/src/main/java/com/algonquin/androidfinalproject/MainActivity.java
@@ -1,7 +1,10 @@
 package com.algonquin.androidfinalproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startOCTranspo = (Button)findViewById(R.id.startOCTranspo);
+        startOCTranspo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, OCTranspo.class);
+                MainActivity.this.startActivity(intent);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/algonquin/androidfinalproject/OCTranspo.java
@@ -0,0 +1,19 @@
+package com.algonquin.androidfinalproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.widget.Button;
+
+public class OCTranspo extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_octranspo);
+
+        Button button = (Button)findViewById(R.id.button);
+
+        //Snackbar.make(button, "String to show", Snackbar.LENGTH_LONG).show();
+    }
+}
